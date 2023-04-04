3c0ad33afb6600e968de002d2a6b6be1c33d6ed0
Saranyavv4 <saranya.v.v98@gmail.com>
2023-04-04 22:19:54 +0530

    completed

--- script.js
+++ script.js
@@ -1,50 +1,29 @@
 const navbar = document.querySelector('.header .navbar');
 const menuButton = document.querySelector('.header .menu');
 
-const myBtn = document.getElementById("myButton")
-const myMenu = document.getElementById("myNav")
-var isMenuShow = false;
-myBtn.addEventListener('click',()=>{
-    myBtn.classList.toggle('fa-close');
-    if(!isMenuShow){
-        isMenuShow = true;
-        myMenu.style.visibility = "visible";
-        //myMenu.style.opacity = "1"
+
+document.onscroll = () => {
+    const menuToggle = document.getElementById("menu-toggle")
+    const mn = document.getElementById("menu-main")
+    if(menuToggle.checked){
+        menuToggle.checked = false
+        mn.style.height = "0px"
+        mn.style.transition = "height 400ms cubic-bezier(0.23, 1, 0.32, 1)"
+    }
+}
+
+function goTo(service){
+    document.location.href = '/'+service+'.html'
+}
+
+function checkMenu(){
+    var chk = document.getElementById("menu-toggle").checked
+    const mn = document.getElementById("menu-main")
+    if(chk){
+        mn.style.height = "30em"
+        mn.style.transition = "height 400ms cubic-bezier(0.23, 1, 0.32, 1)"
     }else{
-        isMenuShow = false;
-        myMenu.style.visibility = "hidden";
-        //myMenu.style.opacity = "0"
+        mn.style.height = "0px"
+        mn.style.transition = "height 400ms cubic-bezier(0.23, 1, 0.32, 1)"
     }
-})
-
-
-/*menuButton.addEventListener('click', ()=>{
-    //var myNav = document.getElementById("myNav")
-    //var myBtn = document.getElementById("myButton")
-    navbar.classList.remove('show');
-    myBtn.classList.toggle('fa-close');
-})*/
-
-document.onscroll = () =>{
-    //navbar.classList.remove('show');
-    if(isMenuShow){
-        isMenuShow = false;
-        myMenu.style.visibility = "hidden";
-    }
-    menuButton.classList.remove('fa-close');
-
-    if (window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
-};
-
-
-document.onload = () =>{
-    if (window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
-};
+}
